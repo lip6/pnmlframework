bfdee9bc6ce234dc6708c22082aee85e92f0e4ee
Lom Messan Hillah <lhillah@gmail.com>
2014-01-31 11:04:24 +0100

    new copyright header

--- pnmlFw-Utils/src/fr/lip6/move/pnml/framework/utils/IdRepository.java
+++ pnmlFw-Utils/src/fr/lip6/move/pnml/framework/utils/IdRepository.java
@@ -1,12 +1,15 @@
 /**
- *  Copyright 2009 Sorbonne Universités, Université Pierre & Marie Curie - CNRS UMR 7606 (LIP6/MoVe)
+ *  Copyright 2009 Universite Paris Ouest Nanterre & Sorbonne Universites, Univ. Paris 06 - CNRS UMR 7606 (LIP6/MoVe)
  *  All rights reserved.   This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
  *  http://www.eclipse.org/legal/epl-v10.html
  *
- *  Initial contributor:
+ *  Project leader / Initial Contributor:
  *    Lom Messan Hillah - <lom-messan.hillah@lip6.fr>
+ *
+ *  Contributors:
+ *    ${ocontributors} - <$oemails}>
  *
  *  Mailing list:
  *    lom-messan.hillah@lip6.fr
