d359cfd6bf2c024e096329296def46401a3b3746
Lom Messan Hillah <lhillah@gmail.com>
2014-12-30 19:04:27 +0100

    switched to slf4j + logback; deprecated methods that provide commoon loggings

--- pnmlFw-Utils/src/fr/lip6/move/pnml/framework/utils/logging/LogMaster.java
+++ pnmlFw-Utils/src/fr/lip6/move/pnml/framework/utils/logging/LogMaster.java
@@ -24,6 +24,8 @@ package fr.lip6.move.pnml.framework.utils.logging;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This utility class is used for logging access and config.
@@ -32,99 +34,101 @@ import org.apache.commons.logging.LogFactory;
  *
  */
 public final class LogMaster {
-	 /**
-     * The logger class.
-     */
-    private static String logger = "org.apache.commons.logging.impl.SLF4JLog";
-    //"org.apache.commons.logging.impl.SimpleLog";
-    /**
-     * the level of logging.
-     */
-    private static String level = "info";
+	/**
+	 * The logger class.
+	 */
+	private static String logger = "org.apache.commons.logging.impl.SLF4JLog";
+	// "org.apache.commons.logging.impl.SimpleLog";
+	/**
+	 * the level of logging.
+	 */
+	private static String level = "info";
 
-    
 	static {
-		 // set the logger class
-        System.setProperty("org.apache.commons.logging.Log", logger);
-        System.setProperty("org.apache.commons.logging.simplelog.defaultlog",
-                level);
-        System.setProperty("org.apache.commons.logging.simplelog.showdatetime",
-                "false");
-        System.setProperty("org.apache.commons.logging.simplelog.showlogname",
-                "true");
-        System
-                .setProperty(
-                        "org.apache.commons.logging.simplelog.showShortLogname",
-                        "true");
-        System
-        .setProperty(
-                "org.slf4j.simpleLogger.levelInBrackets",
-                "true");
+		// set the logger class
+		System.setProperty("org.apache.commons.logging.Log", logger);
+		System.setProperty("org.apache.commons.logging.simplelog.defaultlog", level);
+		System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "false");
+		System.setProperty("org.apache.commons.logging.simplelog.showlogname", "true");
+		System.setProperty("org.apache.commons.logging.simplelog.showShortLogname", "true");
+		System.setProperty("org.slf4j.simpleLogger.levelInBrackets", "true");
 
-        System.setProperty(
-                "org.apache.commons.logging.simplelog.dateTimeFormat",
-                "yyyy/MM/dd HH:mm:ss");
+		System.setProperty("org.apache.commons.logging.simplelog.dateTimeFormat", "yyyy/MM/dd HH:mm:ss");
 	}
-    /**
-     * The singleton instance.
-     */
-    private static LogMaster instance;
 
-   
-  
-    /**
-     * Set all default values for logger, using user set ones if any.
-     */
-    private LogMaster() {
-       
-    }
+	/**
+	 * The singleton instance.
+	 */
+	private static LogMaster instance;
 
-    /**
-     * Set the logging level for future logger, Be careful this destroy the
-     * singleton.
-     *
-     * @param myLevel
-     *            debug, error...
-     * @return the old level
-     */
-    public static String setLevel(String myLevel) {
-        final String old = level;
-        level = myLevel;
-        instance = null; // NOPMD by ggiffo on 7/21/08 5:00 PM
-        return old;
-    }
+	/**
+	 * Set all default values for logger, using user set ones if any.
+	 */
+	private LogMaster() {
 
-    /**
-     * Set the logging classes for future logger, Be careful this destroy the
-     * singleton. May be used before any log instanciation.
-     *
-     * @param mylogger
-     *            the class to logger
-     * @return the old logger
-     */
-    public static String setlogger(String mylogger) {
-        final String old = logger;
-        logger = mylogger;
-        instance = null; // NOPMD by ggiffo on 7/21/08 5:00 PM
-        return old;
-    }
+	}
 
-    /**
-     * Give a logger.
-     *
-     * @param name
-     *            The logger name identification
-     * @return a Logger
-     * @see org.apache.commons.logging.Log
-     */
-    public static synchronized Log giveLogger(String name) { // NOPMD by
-        // ggiffo on
-        // 7/21/08 5:00
-        // PM
-        if (instance == null) {
-            instance = new LogMaster();
-        }
-        return LogFactory.getFactory().getInstance(name);
-    }
+	/**
+	 * Set the logging level for future logger, Be careful this destroys the
+	 * singleton.
+	 *
+	 * @param myLevel
+	 *            debug, error...
+	 * @return the old level
+	 * @deprecated
+	 */
+	public static String setLevel(String myLevel) {
+		final String old = level;
+		level = myLevel;
+		System.setProperty("org.apache.commons.logging.simplelog.defaultlog", level);
+		instance = null; // NOPMD by ggiffo on 7/21/08 5:00 PM
+		return old;
+	}
 
+	/**
+	 * Set the logging classes for future logger, Be careful this destroys the
+	 * singleton. May be used before any log instanciation.
+	 *
+	 * @param mylogger
+	 *            the class to logger
+	 * @return the old logger
+	 * @deprecated
+	 */
+	public static String setlogger(String mylogger) {
+		final String old = logger;
+		logger = mylogger;
+		instance = null; // NOPMD by ggiffo on 7/21/08 5:00 PM
+		return old;
+	}
+
+	/**
+	 * Gives a logger.
+	 *
+	 * @param name
+	 *            The logger name identification
+	 * @return a Logger
+	 * @see org.apache.commons.logging.Log
+	 * @deprecated @see {{@link #getLogger(String)}
+	 */
+	public static synchronized Log giveLogger(String name) { // NOPMD by
+		// ggiffo on
+		// 7/21/08 5:00
+		// PM
+		if (instance == null) {
+			instance = new LogMaster();
+		}
+		return LogFactory.getFactory().getInstance(name);
+	}
+
+	/**
+	 * Provides a logger.
+	 *
+	 * @param name
+	 *            The logger name identification
+	 * @return a Logger
+	 * @see org.slf4j.Logger
+	 */
+	public static synchronized Logger getLogger(String name) {
+		return LoggerFactory.getLogger(name);
+	}
 }
