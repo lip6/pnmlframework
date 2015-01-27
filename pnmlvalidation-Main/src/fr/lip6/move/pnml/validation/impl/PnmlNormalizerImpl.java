19fe815af19651ba56e4f94d27befadaac1c7179
Lom Messan Hillah <lhillah@gmail.com>
2015-01-27 23:16:43 +0100

    new feature to normalize PNML Document (merging parallel arcs is supported)

--- pnmlvalidation-Main/src/fr/lip6/move/pnml/validation/impl/PnmlNormalizerImpl.java
+++ pnmlvalidation-Main/src/fr/lip6/move/pnml/validation/impl/PnmlNormalizerImpl.java
@@ -20,6 +20,7 @@
 package fr.lip6.move.pnml.validation.impl;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -29,7 +30,10 @@ import fr.lip6.move.pnml.framework.utils.logging.LogMaster;
 import fr.lip6.move.pnml.ptnet.Arc;
 import fr.lip6.move.pnml.ptnet.Page;
 import fr.lip6.move.pnml.ptnet.PetriNet;
+import fr.lip6.move.pnml.ptnet.hlapi.ArcHLAPI;
+import fr.lip6.move.pnml.ptnet.hlapi.PageHLAPI;
 import fr.lip6.move.pnml.ptnet.hlapi.PetriNetDocHLAPI;
+import fr.lip6.move.pnml.ptnet.hlapi.PetriNetHLAPI;
 import fr.lip6.move.pnml.validation.CheckPnmlFile;
 import fr.lip6.move.pnml.validation.PnmlNormalizer;
 
@@ -49,7 +53,7 @@ public final class PnmlNormalizerImpl implements PnmlNormalizer {
 
 	private Logger log;
 
-	private List<List<Object>> parArcs;
+	private List<List<ArcHLAPI>> ptParArcs;
 
 	public PnmlNormalizerImpl(CheckPnmlFile checker) {
 		this.pchecker = checker;
@@ -62,7 +66,7 @@ public final class PnmlNormalizerImpl implements PnmlNormalizer {
 		assignCheckerToThis(checker);
 		HLAPIRootClass pdoc = checker.getPnmlDocHLAPIRootClass();
 		if (pdoc == null) {
-			log.error("You have not load any PNML Document through the PNML Checker."
+			log.error("You have not loaded any PNML Document through the PNML Checker."
 					+ " Please load first a PNML Document through the PNML Checker");
 
 		} else {
@@ -73,75 +77,89 @@ public final class PnmlNormalizerImpl implements PnmlNormalizer {
 
 	private boolean collectParallelArcs(HLAPIRootClass pdoc) {
 		boolean res = false;
-		if (parArcs == null)
-			parArcs = new ArrayList<>();
+		if (ptParArcs == null)
+			ptParArcs = new ArrayList<>();
 		else
-			parArcs.clear();
-		List<Object> localCol;
+			ptParArcs.clear();
 		if (this.pchecker.isPTNetDocument()) {
-			PetriNetDocHLAPI pnDoc = (PetriNetDocHLAPI) pdoc;
-			for (PetriNet pn : pnDoc.getNets()) {
-				for (Page pg : pn.getPages()) {
-					fr.lip6.move.pnml.ptnet.Arc[] arcs = (Arc[]) pg.getObjects().parallelStream()
-							.filter(pno -> pno.getClass().getCanonicalName().contains("Arc")).toArray();
-					if (arcs != null && arcs.length != 0) {
-						for (int i = 0; i < arcs.length - 1; i++) {
-							localCol = new ArrayList<>();
-							localCol.add(arcs[i]);
-							for (int j = i + 1; j < arcs.length; j++) {
-								if (arcs[i].getSource().getId().equals(arcs[j].getSource().getId())
-										&& arcs[i].getTarget().getId().equals(arcs[j].getTarget().getId())) {
-									localCol.add(arcs[j]);
-								}
-								if (localCol.size() > 1) {
-									parArcs.add(localCol);
-								} else {
-									localCol.clear();
-									localCol = null;
-								}
-
-							}
-						}
-
-					}
-				}
-			}
+			res = findParallelArcsInPT(pdoc);
 
 		} else {
 			log.error("Only P/T Nets are currently supported in searching for parallel arcs.");
 		}
-		if (parArcs.size() > 0) {
-			res = true;
-		}
 		return res;
+	}
+
+	private boolean findParallelArcsInPT(HLAPIRootClass pdoc) {
+		boolean found = false;
+		List<ArcHLAPI> localCol;
+		ArcHLAPI a1, a2;
+		PetriNetDocHLAPI pnDoc = (PetriNetDocHLAPI) pdoc;
+		for (PetriNetHLAPI pn : pnDoc.getNetsHLAPI()) {
+			log.info("Looking for parallel arcs in net " + pn.getId());
+			for (PageHLAPI pg : pn.getPagesHLAPI()) {
+				List<ArcHLAPI> arcs = pg.getObjects_ArcHLAPI();
+				if (arcs != null && arcs.size() > 0) {
+					Iterator<ArcHLAPI> iter1 = arcs.listIterator();
+					Iterator<ArcHLAPI> iter2;
+					while (iter1.hasNext()) {
+						a1 = iter1.next();
+						localCol = new ArrayList<>();
+						localCol.add(a1);
+						iter1.remove();
+						iter1 = null;
+						iter2 = arcs.listIterator();
+						while (iter2.hasNext()) {
+							a2 = iter2.next();
+							if (a1.getSource().getId().equals(a2.getSource().getId())
+									&& a1.getTarget().getId().equals(a2.getTarget().getId())) {
+								localCol.add(a2);
+								iter2.remove();
+							}
+						}
+						if (localCol.size() > 1) {
+							ptParArcs.add(localCol);
+						} else {
+							localCol.clear();
+							localCol = null;
+						}
+						iter2 = null;
+						iter1 = arcs.listIterator();
+					}
+				}
+			}
+		}
+		if (ptParArcs.size() > 0) {
+			found = true;
+		}
+		return found;
 	}
 
 	@Override
 	public String reportParallelArcs(CheckPnmlFile checker) {
 		assignCheckerToThis(checker);
 		StringBuilder sb = new StringBuilder();
-		if (parArcs == null) {
+		// TODO upgrade expression when SN are supported.
+		if (ptParArcs == null) {
 			checkCanMergeParallelArcs(checker);
 		}
 		int i = 1;
-		if (parArcs != null && parArcs.size() > 0) {
-			if (this.pchecker.isPTNetDocument()) {
-				sb.append("The following " + parArcs.size() + " sets of arcs have been found:" + NL);
-				for (List<Object> lo : parArcs) {
+		if (this.pchecker.isPTNetDocument()) {
+			if (ptParArcs != null && ptParArcs.size() > 0) {
+				sb.append("The following " + ptParArcs.size() + " sets of parallel arcs have been found:" + NL);
+				for (List<ArcHLAPI> la : ptParArcs) {
 					sb.append("Set " + i++ + NL);
-					for (Object o : lo) {
-						fr.lip6.move.pnml.ptnet.Arc arc = (fr.lip6.move.pnml.ptnet.Arc) o;
-						sb.append(TAB).append(arc.getId()).append(": ").append(arc.getSource().getId()).append(" -> ")
-								.append(arc.getTarget().getId()).append(NL);
+					for (ArcHLAPI o : la) {
+						sb.append(TAB).append(o.getId()).append(": ").append(o.getSource().getId()).append(" -> ")
+								.append(o.getTarget().getId()).append(NL);
 					}
 					sb.append(NL);
 				}
 			} else {
-				log.error("Only P/T Nets are currently supported in searching for parallel arcs.");
+				sb.append("There are no parallel arcs to report in this net.");
 			}
-
 		} else {
-			sb.append("There is no parallel arcs to report in this net.");
+			sb.append("Only P/T Nets are currently supported in searching for parallel arcs.");
 		}
 		return sb.toString();
 	}
@@ -149,7 +167,44 @@ public final class PnmlNormalizerImpl implements PnmlNormalizer {
 	@Override
 	public HLAPIRootClass mergeParallelArcs(CheckPnmlFile checker) {
 		assignCheckerToThis(checker);
-		return null;
+		// TODO upgrade expression when SN are supported.
+		if (ptParArcs == null) {
+			checkCanMergeParallelArcs(checker);
+		}
+		StringBuilder sb = new StringBuilder();
+		int i = 1, insc;
+		if (this.pchecker.isPTNetDocument()) {
+			if (ptParArcs != null && ptParArcs.size() > 0) {
+				ArcHLAPI a1, a2;
+				Page pg;
+				for (List<ArcHLAPI> la : ptParArcs) {
+					sb.append("Set " + i++ + NL);
+					a1 = la.get(0);
+					sb.append(TAB).append(a1.getId()).append(": ").append(a1.getSource().getId()).append(" -> ")
+					.append(a1.getTarget().getId()).append(NL);
+					insc = a1.getInscription().getText();
+					for (int j = 1; j < la.size(); j++) {
+						a2 = la.get(j);
+						sb.append(TAB).append(a2.getId()).append(": ").append(a2.getSource().getId()).append(" -> ")
+								.append(a2.getTarget().getId()).append(NL);
+						insc += a2.getInscription().getText();
+						pg = a2.getContainerPage();
+						a2.getContainedItem().setSource(null);
+						a2.getContainedItem().setTarget(null);
+						pg.getObjects().remove(a2.getContainedItem());
+					}
+					a1.getInscription().setText(insc);
+				}
+				log.info("I merged the following " + ptParArcs.size() + " sets of parallel arcs:" + NL);
+				System.out.println(sb.toString());
+				log.info("For each merge, the first arc in the set is kept as the resulting arc.");
+			} else {
+				log.warn("There are no parallel arcs to merge in this net.");
+			}
+		} else {
+			log.error("Only P/T Nets are currently supported in merging parallel arcs.");
+		}
+		return this.pchecker.getPnmlDocHLAPIRootClass();
 	}
 
 	private void assignCheckerToThis(CheckPnmlFile checker) {
