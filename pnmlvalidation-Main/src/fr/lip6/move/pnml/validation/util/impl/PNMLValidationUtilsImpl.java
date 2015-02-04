8615923a20402d4dc56756d9d12979fd207cca9e
Lom Messan Hillah <lhillah@gmail.com>
2015-02-05 00:04:08 +0100

    handled place marking and reverse link from graphics object to container

--- pnmlvalidation-Main/src/fr/lip6/move/pnml/validation/util/impl/PNMLValidationUtilsImpl.java
+++ pnmlvalidation-Main/src/fr/lip6/move/pnml/validation/util/impl/PNMLValidationUtilsImpl.java
@@ -1,3 +1,22 @@
+/**
+ *  Copyright 2009-2015 Université Paris Ouest and Sorbonne Universités,
+ * 							Univ. Paris 06 - CNRS UMR
+ * 							7606 (LIP6)
+ *
+ *  All rights reserved.   This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Project leader / Initial Contributor:
+ *    Lom Messan Hillah - <lom-messan.hillah@lip6.fr>
+ *
+ *  Contributors:
+ *    L.M. Hillah - <$oemails}>
+ *
+ *  Mailing list:
+ *    lom-messan.hillah@lip6.fr
+ */
 package fr.lip6.move.pnml.validation.util.impl;
 
 import org.slf4j.Logger;
@@ -48,31 +67,53 @@ public final class PNMLValidationUtilsImpl implements PNMLValidationUtils {
 			for (PetriNetHLAPI pn : pnDoc.getNetsHLAPI()) {
 				log.info("Looking for graphical information in net " + pn.getId());
 				if (pn.getName() != null && pn.getName().getAnnotationgraphics() != null) {
+					pn.getName().getAnnotationgraphics().setContainerAnnotation(null);
 					pn.getName().setAnnotationgraphics(null);
+					res = true;
 				}
 				for (PageHLAPI pg : pn.getPagesHLAPI()) {
 					if (pg.getNodegraphics() != null) {
+						pg.getNodegraphics().setContainerPage(null);
 						pg.setNodegraphicsHLAPI(null);
+						res = true;
 					}
 					if (pg.getName() != null && pg.getName().getAnnotationgraphics() != null) {
+						pg.getName().getAnnotationgraphics().setContainerAnnotation(null);
 						pg.getName().setAnnotationgraphics(null);
+						res = true;
 					}
 					for (PnObject pno : pg.getObjects()) {
 						if (pno.getName() != null && pno.getName().getAnnotationgraphics() != null) {
+							pno.getName().getAnnotationgraphics().setContainerAnnotation(null);
 							pno.getName().setAnnotationgraphics(null);
+							res = true;
 						}
-						if (pno.getClass().isInstance(fr.lip6.move.pnml.ptnet.Node.class)) {
+						if (pno instanceof fr.lip6.move.pnml.ptnet.Node) {
 							fr.lip6.move.pnml.ptnet.Node node = (fr.lip6.move.pnml.ptnet.Node) pno;
 							if (node.getNodegraphics() != null) {
+								node.getNodegraphics().setContainerNode(null);
 								node.setNodegraphics(null);
+								res = true;
 							}
-						} else if (pno.getClass().isInstance(fr.lip6.move.pnml.ptnet.Arc.class)) {
+							if (pno instanceof fr.lip6.move.pnml.ptnet.Place) {
+								fr.lip6.move.pnml.ptnet.Place place = (fr.lip6.move.pnml.ptnet.Place)pno;
+								if (place.getInitialMarking() != null && place.getInitialMarking().getAnnotationgraphics() != null) {
+									place.getInitialMarking().getAnnotationgraphics().setContainerAnnotation(null);
+									place.getInitialMarking().setAnnotationgraphics(null);
+									res = true;
+								}
+							}
+						} else if (pno instanceof fr.lip6.move.pnml.ptnet.Arc) {
 							fr.lip6.move.pnml.ptnet.Arc arc = (fr.lip6.move.pnml.ptnet.Arc) pno;
 							if (arc.getArcgraphics() != null) {
+								arc.getArcgraphics().setContainerArc(null);
 								arc.setArcgraphics(null);
+								res = true;
 							}
 							if (arc.getInscription() != null && arc.getInscription().getAnnotationgraphics() != null) {
+								arc.getInscription().getAnnotationgraphics().setContainerAnnotation(null);
 								arc.getInscription().setAnnotationgraphics(null);
+								res = true;
 							}
 						} else {
 							log.error("The graphical information removal feature currently supports Nodes (types of Place and Transitions), Arcs, and their subtypes only. "
